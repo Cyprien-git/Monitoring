918113104fa282470d99d898dc61f04fb37e8c56
Cyprien-git <cyprienpierre.viard@gmail.com>
2026-02-28 21:20:21 +0100

    add Disk monitoring

--- api/pyflux.py
+++ api/pyflux.py
@@ -17,17 +17,19 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 
 async def monitor_task():
     while True:
-        cpu = psutil.cpu_percent(interval=None) 
+        cpu = psutil.cpu_percent(interval=None)
         ram = psutil.virtual_memory().percent
+        disk = psutil.disk_usage('/').percent
         
         point = Point("system_stats") \
             .tag("host", "raspberry") \
             .field("cpu_usage", cpu) \
-            .field("ram_usage", ram)
+            .field("ram_usage", ram) \
+            .field("disk_usage", disk)
 
         try:
             write_api.write(bucket=BUCKET, org=ORG, record=point)
-            print(f"✅ API Sent: CPU {cpu}%")
+            print(f"✅ API Sent: CPU {cpu}% | RAM {ram}% | DISK {disk}%")
         except Exception as e:
             print(f"❌ Error: {e}")
 
